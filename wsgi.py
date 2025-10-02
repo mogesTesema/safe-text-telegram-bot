ff458142438971b05b0e1b9082eb50ce32b45d0f
Moges-Tesema <moges2421@gmail.com>
2025-10-02 14:31:39 +0300

    add self_ping function to call to itself to not sleep

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,4 @@
-import os
-import threading
+import requests, threading, time, os
 from flask import Flask
 from bot import main  # import your main function from bot.py
 
@@ -9,8 +8,20 @@ app = Flask(__name__)
 def home():
     return "✅ Safe Text API Bot is running on Render!"
 
+def self_ping():
+    url = "https://safe-text-telegram-bot-1.onrender.com"
+    if not url:
+        return
+    while True:
+        try:
+            requests.get(url)
+        except Exception as e:
+            print("Ping failed:", e)
+        time.sleep(600) 
+
 # Run your Telegram bot in a background thread
 threading.Thread(target=main, daemon=True).start()
+threading.Thread(target=self_ping,daemon=True).start()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
